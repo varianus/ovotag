3c705d99c635dbbdf4900c9710967096a646596d
varianus <varianus@users.noreply.github.com>
2021-12-31 18:28:57 +0100

    Add a new unit to register all tag readers Remove useless dependecies

--- packages/ovotag.pas
+++ packages/ovotag.pas
@@ -9,9 +9,9 @@ interface
 
 uses
   AudioTag, basetag, CommonFunctions, file_Dummy, file_flac, file_Monkey, 
-  file_mp3, file_Mp4, file_ogg, file_opus, file_Wave, file_wma, ID3v1Genres, 
+  file_mp3, file_Mp4, file_ogg, file_opus, file_wave, file_wma, ID3v1Genres, 
   song, tag_APE, tag_Dummy, tag_id3v2, tag_MP4, tag_vorbis, tag_wma, 
-  LazarusPackageIntf;
+  RegisterAllReader, LazarusPackageIntf;
 
 implementation
 
